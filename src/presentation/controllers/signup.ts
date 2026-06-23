20e0a2a918b22a86db945920e18398fe6e171878
Arthur Resende <arthur.resendecoura@hotmail.com>
2026-06-23 17:02:57 -0300

    feat: ensure `SignUpController` returns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any -- At this point the implementation is not complete */
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access -- At this point the implementation is not complete */
+
 export class SignUpController {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- At this point the implementation is not complete
   handle(httpRequest: any): any {
-    return {
-      body: new Error('Missing param: name'),
-      statusCode: 400,
+    if (!httpRequest.body.name) {
+      return {
+        body: new Error('Missing param: name'),
+        statusCode: 400,
+      };
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        body: new Error('Missing param: email'),
+        statusCode: 400,
+      };
     }
   }
 }
